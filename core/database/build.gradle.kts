34c2c49d9799bca81238164b98f4ace68eb17c2b
Willie Chalmers III <willie@williecubed.me>
2024-03-27 21:08:38 -0700

    Save database schema to file, update dependencies

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -7,12 +7,18 @@ android {
     namespace = "app.logdate.core.database"
 }
 
+ksp {
+    arg("room.schemaLocation", "$projectDir/schemas")
+}
+
 dependencies {
     implementation(project(":core:model"))
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
 
     // Unit testing
     testImplementation(libs.junit)
+    testImplementation(libs.androidx.room.testing)
     testImplementation(libs.kotlinx.coroutines.test)
 }
