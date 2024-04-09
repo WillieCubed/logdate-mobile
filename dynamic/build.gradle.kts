ff200afeed19f2953fea5adff570af6b467413be
Willie Chalmers III <willie@williecubed.me>
2024-04-09 02:09:18 -0700

    Clean up Google Play Instant dynamic module

--- dynamic/build.gradle.kts
+++ dynamic/build.gradle.kts
@@ -1,10 +1,9 @@
-@Suppress("DSL_SCOPE_VIOLATION") // Remove when fixed https://youtrack.jetbrains.com/issue/KTIJ-19369
 plugins {
     alias(libs.plugins.logdate.dynamic)
 }
 
 android {
-    namespace = "app.logdate.mobile.dynamic"
+    namespace = "app.logdate.mobile.instant"
 }
 
 dependencies {
